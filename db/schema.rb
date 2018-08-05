2ab57c86debb910829d3b7449b3a5995a5d4201c
Michael Mahaffey <mayhaffs@gmail.com>
2018-08-05 00:39:28 -0500

    add Ingredients scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_012359) do
+ActiveRecord::Schema.define(version: 2018_08_05_053600) do
 
   create_table "cookbooks", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
