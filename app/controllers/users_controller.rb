9af7cc6319a23bd69f2a6bd079385842dde75bad
Justin White <Jjustinwhite@mac.com>
2018-08-05 13:56:11 -0500

    basic login/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-       flash[:success] = "Welcome to Cookbooks!"
+      log_in @user
+      flash[:success] = "Welcome to Cookbooks!"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'home#index'
 
   get  'users/new'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :ingredients
   resources :recipes
