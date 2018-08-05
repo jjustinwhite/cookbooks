25ee0164fd7013a0a891f11973b8ab38272f2f19
Luke Kennin <lkmeq@me.com>
2018-08-04 21:01:01 -0500

    fix duplicate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cookbook.destroy_all
+Recipe.destroy_all
+
 
 Cookbook.create([{
   name: "Fresh Juice"
