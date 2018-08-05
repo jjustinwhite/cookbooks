c91d99575bab0b800bb4bd52245d67e40cb87505
Justin White <Jjustinwhite@mac.com>
2018-08-05 14:16:01 -0500

    remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_171916) do
+ActiveRecord::Schema.define(version: 2018_08_05_185645) do
 
   create_table "cookbooks", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_171916) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
